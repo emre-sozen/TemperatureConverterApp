2f4c5f8cd8299016411d0ef22de4c0b3d7cee50c
sozen-emre <emre.sozen@resinasoft.com>
2022-06-08 21:48:33 +0300

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Emre Sözen on 8.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
