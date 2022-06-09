18d8f74b19c3601afd36d8c7e005d13397ebaaa1
sozen-emre <emre.sozen@resinasoft.com>
2022-06-09 19:39:25 +0300

    Converter Celsius Feature

--- TemperatureConverter/ContentView.swift
+++ TemperatureConverter/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var fahrenheitValue: String = ""
+    
+    let numberFormatter: NumberFormatter = {
+       let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.maximumFractionDigits = 2
+        return numberFormatter
+    }()
+    
+    func convertToCelsius() -> String {
+        if let value = Double(fahrenheitValue) {
+            let fahrenheit = Measurement<UnitTemperature>(value: value, unit: .fahrenheit)
+            
+            let celsiusValue = fahrenheit.converted(to: .celsius)
+            return numberFormatter.string(from: NSNumber(value: celsiusValue.value)) ?? "???"
+        } else {
+            return "???"
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("value", text: $fahrenheitValue)
+                .keyboardType(.decimalPad)
+                .font(Font.system(size: 64.0))
+                .multilineTextAlignment(.center)
+            Text("fahrenheit")
+            Text("is actually")
+                .foregroundColor(.gray)
+            Text(convertToCelsius())
+                .font(Font.system(size: 64.0))
+            Text("Degrees Celcius")
+            Spacer()
+        }
+        .foregroundColor(.orange)
+        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
     }
 }
 
